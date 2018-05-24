de3d45e84a92a903f983fccc06165a63ad9beb5b
klimsava <klimsava96@mail.ru>
2018-05-24 18:01:47 +0300

    klim
    new Test site

---
+++ test/app/js/common.js
@@ -0,0 +1,14 @@
+$(function() {
+
+$('.carousel-trevel').owlCarousel({
+	loop: true,
+	nav: true,
+	smartSpeed: 700,
+	navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+	items: 1
+});
+
+
+
+
+});
