e71d69453542eb4bff8f54472bcac9d0e427dc46
klimsava <klimsava96@mail.ru>
2018-05-29 00:39:43 +0300

    klim
    new file

--- test/app/js/common.js
+++ test/app/js/common.js
@@ -8,7 +8,36 @@ $('.carousel-trevel').owlCarousel({
 	items: 1
 });
 
+$('#videoimg, .company-video__link').click(function(){
+var height=$('#videoimg').height(),
+	width =$('#videoimg').width();
+$('.company-video').html('<iframe width="'+ width +'" height="' + height + '" src="https://www.youtube.com/embed/rXLmt6i-hBY?rel=0&autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+});
 
+$('.header-btn__link').click(function() {
+$('html, body').animate({scrollTop: $('.section-description').offset().top }, 'slow');
+	return false;
+});
 
 
 });
+
+$(document).ready( function() {
+	var fsvs = $.fn.fsvs({
+		speed : 5000,
+		bodyID : 'fsvs-body',
+		selector : '> .slide',
+		mouseSwipeDisance : 40,
+		afterSlide : function(){},
+		beforeSlide : function(){},
+		endSlide : function(){},
+		mouseWheelEvents : true,
+		mouseDragEvents : true,
+		touchEvents : true,
+		arrowKeyEvents : true,
+		pagination : true,
+		nthClasses : false,
+		detectHash : true
+	});
+});
+
