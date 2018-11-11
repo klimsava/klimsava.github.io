3c635d5bca42ad6fed27bca405492d2dde6e1862
Klim Savanovich <Klimsava96@maill.ru>
2018-11-11 14:52:38 +0300

    klimsava
    new

---
+++ velox/js/commond.js
@@ -0,0 +1,28 @@
+$(function() {
+    $(".baner-slider").slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></button>'
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('#scroller').fadeIn();
+        } else {
+            $('#scroller').fadeOut();
+        }
+    });
+    $('#scroller').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+
+    $('.menu-btn').on('click', function (e) {
+        e.preventDefault;
+        $(this).toggleClass('menu-btn--active');
+        $('.menu-nav').toggleClass('menu-nav--active');
+    });
+
+});
